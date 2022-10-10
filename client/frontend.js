39ed781aa64090b60eca066ba1514611d37efbb3
Rostyslav <rostikbovonenko@gmail.com>
2022-10-10 13:31:30 +0200

    added get, post, delete, put methods

--- client/frontend.js
+++ client/frontend.js
@@ -1,16 +1,24 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.10/dist/vue.esm.browser.js';
 
+Vue.component('loader', {
+    template: `
+        <div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    `
+})
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
             },
-            contacts: [
-                {id: 1, name: 'Rostyslav', value: '+420774233299', marked: false}
-            ]
+            contacts: []
         }
     },
     computed: {
@@ -19,19 +27,53 @@ new Vue({
         }
     },
     methods: {
-        createContact() {
+        async createContact() {
             const {...contact} = this.form;
 
-            this.contacts.push({...contact, id: Date.now(), marked: false});
+            const newContact = await request('/api/contacts', 'POST', contact);
+            
+            this.contacts.push(newContact);
 
             this.form.name = this.form.value = '';
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find(contact => contact.id === id);
-            contact.marked = true;
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true
+            });
+            contact.marked = updated.marked;
         },
-        removeContact(id) {
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE');
             this.contacts = this.contacts.filter(contact => contact.id !== id);
         }
+    },
+    async mounted() {
+        this.loading = true;
+        this.contacts = await request('/api/contacts');
+        this.loading = false;
     }
 })
+
+async function request (url, method = "GET", data = null) {
+    try {
+        const headers = {};
+        let body;
+
+        if (data) {
+            headers['Content-Type'] = 'application/json';
+            body = JSON.stringify(data);
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        });
+        return await response.json();
+        
+    } catch (e) {
+        throw new Error(`Could not fetch ${url}, status: ${e.message}` );
+    }
+}
